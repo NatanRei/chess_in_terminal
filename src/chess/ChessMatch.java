9af7be1008fa941146a9b7f4fc59e212e84fade9
NatanRei <nreischmura@gmail.com>
2023-07-13 22:57:19 -0300

    feat: little improvent board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,7 +2,8 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Position;
-import chess.pieces.Hook;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -29,7 +30,18 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('a', 8, new Hook(board, Color.WHITE));
-		placeNewPiece('a', 1, new Hook(board, Color.BLACK));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
